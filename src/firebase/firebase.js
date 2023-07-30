15b46272b2abcf4f4e70cc06f55b940308123e39
hyoloui <tmdgy9272@gmail.com>
2023-07-30 17:37:53 +0900

    feat: firebase ( 파이어베이스 설정 )
    - google.firebase 를 초기세팅 합니다.
    - auth/firestore/storage 사용

---
+++ src/firebase/firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFireStore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD3xSnIPISWMMrtEK73_Olwp4SKKusJz3I",
+  authDomain: "nextjs-shop-ing.firebaseapp.com",
+  projectId: "nextjs-shop-ing",
+  storageBucket: "nextjs-shop-ing.appspot.com",
+  messagingSenderId: "208412951347",
+  appId: "1:208412951347:web:35aec2c65583a2c30fb446",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFireStore(app);
+export const sotrage = getStorage(app);
+
+export default app;
