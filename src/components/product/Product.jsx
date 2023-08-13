2e741cd95bd075252c761008720ba12159a7f381
hyoloui <tmdgy9272@gmail.com>
2023-08-13 21:41:22 +0900

    feat: Product 데이터 Redux Store에 저장하기
    - 데이터를 store에 저장 합니다.
    - selectProduct 함수를 이용해 state.product.products를 가져옵니다.

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,11 +1,49 @@
 "use client";
 
+import styles from "./Product.module.scss";
+
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  GET_PRICE_RANGE,
+  STORE_PRODUCTS,
+  selectProducts,
+} from "@/redux/slice/productSlice";
+
 import useFetchCollection from "@/hooks/useFetchCollection";
+
+import Loader from "@/components/loader/Loader";
+import ProductList from "@/components/product/productList/ProductList";
 
 const Product = () => {
   const { data, isLoading } = useFetchCollection("products");
 
-  return <div>Product</div>;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(
+      STORE_PRODUCTS({
+        products: data,
+      })
+    );
+
+    dispatch(
+      GET_PRICE_RANGE({
+        products: data,
+      })
+    );
+  }, [data, dispatch]);
+
+  const products = useSelector(selectProducts);
+
+  return (
+    <section className={styles.product}>
+      <aside className={styles.filter}></aside>
+      <div className={styles.content}>
+        {isLoading ? <Loader basic /> : <ProductList products={products} />}
+      </div>
+    </section>
+  );
 };
 
 export default Product;
