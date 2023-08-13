f29001e373416525a843b510cf88671003c9f937
hyoloui <tmdgy9272@gmail.com>
2023-08-13 23:19:47 +0900

    feat: logic ( ProductFilter )
    - 컴포넌트의 로직을 구현합니다.

--- src/components/product/productFilter/ProductFilter.jsx
+++ src/components/product/productFilter/ProductFilter.jsx
@@ -1,6 +1,51 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import {
+  selectProducts,
+  selecMinPrice,
+  selectMaxPrice,
+} from "@/redux/slice/productSlice";
 
 const ProductFilter = () => {
+  const [category, setCategory] = useState("All");
+  const [brand, setBrand] = useState("All");
+  const [price, setPrice] = useState("10000");
+
+  const products = useSelector(selectProducts);
+  const minPrice = useSelector(selecMinPrice);
+  const maxPrice = useSelector(selectMaxPrice);
+  const dispatch = useDispatch();
+
+  const allCategories = [
+    "All",
+    ...new Set(products.map((product) => product.category)),
+  ];
+
+  const filterCategories = (category) => {
+    setCategory(category);
+    dispatch(FILTER_BY_CATEGORY({ products, category: cat }));
+  };
+
+  const allBrands = [
+    "All",
+    ...new Set(products.map((product) => product.brand)),
+  ];
+
+  const clearFilters = () => {
+    setCategory("All");
+    setBrand("All");
+    setPrice(maxPrice);
+  };
+
+  useEffect(() => {
+    dispatch(FILTER_BY_BRAND({ products, brand }));
+  }, [brand, products, dispatch]);
+
+  useEffect(() => {
+    dispatch(FILTER_BY_PRICE({ products, price }));
+  }, [price, products, dispatch]);
+
   return <div>ProductFilter</div>;
 };
 
