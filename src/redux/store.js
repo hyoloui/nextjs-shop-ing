11ed85a41d13c6c15df845afa13cb9a0a5d21419
hyoloui <tmdgy9272@gmail.com>
2023-08-13 21:39:19 +0900

    feat: 상품 가져오기 ( Slice, Store )
    - 서버통신을 이용해 가져온 데이터를 클라이언트 상태관리를 할 수 있도록 slice를 추가합니다.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,18 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slice/authSlice";
+import productReducer from "./slice/productSlice";
+import cartReducer from "./slice/cartSlice";
+import checkoutReducer from "./slice/checkoutSlice";
+import filterReducer from "./slice/filterSlice";
+import orderReducer from "./slice/orderSlice";
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  product: productReducer,
+  cart: cartReducer,
+  checkout: checkoutReducer,
+  filter: filterReducer,
+  order: orderReducer,
 });
 
 const store = configureStore({ reducer: rootReducer });
