90ea5d165a72bfe934aba28c1f8dc88b9846f75a
hyoloui <tmdgy9272@gmail.com>
2023-08-23 21:38:07 +0900

    feat: 페이지네이션 로직 ( Pagination )
    - 페이지를 컨트롤하는 컴포넌트의 로직을 작성합니다
    - (다음, 이전) 페이지 임계치를 넘어가거나 줄어드는 경우에 따라 처리합니다

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,6 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Pagination = () => {
+const Pagination = ({
+  currentPage,
+  productsPerPage,
+  setCurrentPage,
+  totalProducts,
+}) => {
+  const pageNumbers = [];
+
+  const [pageNumberLimit] = useState(3);
+  const [maxPageNumberLimit, setMaxPageNumberLimit] = useState(3);
+  const [minPageNumberLimit, setMinPageNumberLimit] = useState(0);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  const paginateNextPage = () => {
+    setCurrentPage(currentPage + 1);
+
+    // 최대 페이지 넘버를 넘어가면 최소, 최대 페이지 넘버를 증가시킨다.
+    if (currentPage + 1 > maxPageNumberLimit) {
+      setMaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit);
+      setMinPageNumberLimit(minPageNumberLimit + pageNumberLimit);
+    }
+  };
+
+  const paginatePrevPage = () => {
+    setCurrentPage(currentPage - 1);
+
+    // 최소 페이지 넘버보다 작아지면 최소, 최대 페이지 넘버를 감소시킨다.
+    if ((currentPage - 1) % pageNumberLimit === 0) {
+      setMaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit);
+      setMinPageNumberLimit(minPageNumberLimit - pageNumberLimit);
+    }
+  };
+
+  for (let i = 0; i < Math.ceil(totalProducts / productsPerPage); i++) {
+    pageNumbers.push[i];
+  }
   return <div>Pagination</div>;
 };
 
