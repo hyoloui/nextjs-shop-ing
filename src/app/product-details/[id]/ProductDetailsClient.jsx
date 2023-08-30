fa6653bec2707725411ab7686317c017af406251
hyoloui <tmdgy9272@gmail.com>
2023-08-31 01:34:35 +0900

    feat: 상품 상세 페이지 UI ( ProductDetailsClient )
    - 상품 데이터 나열(제품정보 표시)

--- src/app/product-details/[id]/ProductDetailsClient.jsx
+++ src/app/product-details/[id]/ProductDetailsClient.jsx
@@ -1,18 +1,29 @@
 "use client";
 
-import styles from "./productDetailsClient.module.scss";
+import styles from "./ProductDetails.module.scss";
 
+import { useState } from "react";
+import Image from "next/image";
 import { useParams } from "next/navigation";
-import useFetchDocument from "@/hooks/useFetchDocument";
+import { Rating } from "react-simple-star-rating";
 
-import Loader from "@/components/loader/loader";
+import { priceFormat } from "@/utils/priceFormat";
+import useFetchDocument from "@/hooks/useFetchDocument";
+import Loader from "@/components/loader/Loader";
+import Divider from "@/components/divider/Divider";
+
+import listCashIcon from "@/assets/list-cash-icon.png";
+import Button from "@/components/button/Button";
+import { setIndexConfiguration } from "firebase/firestore";
 
 const ProductDetailsClient = () => {
   const params = useParams();
 
-  const { document: product } = useFetchDocument("productDetail", id);
+  const { document: product } = useFetchDocument("products", params.id);
+  const [count, setCount] = useState(1);
 
   const addToCart = () => {};
+
   const today = new Date();
   const tomorrow = new Date(today.setDate(today.getDate() + 1));
 
@@ -21,7 +32,86 @@ const ProductDetailsClient = () => {
 
   return (
     <section className={styles.product}>
-      {product === null ? <Loader /> : <></>}
+      {product === null ? (
+        <Loader />
+      ) : (
+        <div className={styles.details}>
+          <div className={styles.img}>
+            <Image
+              width={477}
+              height={470}
+              src={product.imageURL}
+              alt={product.name}
+              priority
+            />
+          </div>
+
+          <div className={styles.content}>
+            <div className={styles.header}>
+              <p className={styles.brandName}>{product.brand}</p>
+              <p className={styles.productName}>{product.name}</p>
+              <div className={styles.rating}>
+                <Rating initialValue={3} size={17} />
+                <span className={styles.count}>10,000개 상품평</span>
+              </div>
+            </div>
+
+            <Divider space={0} />
+
+            <div className={styles.container}>
+              <p className={styles.price}>{priceFormat(product.price)}원</p>
+              <div className={styles.rewardCashBadge}>
+                <Image src={listCashIcon} alt="cash-icon" width={12} />
+                <span>최대 {product.price / 10}원 적립</span>
+              </div>
+            </div>
+
+            <Divider space={0} />
+
+            <div className={styles.rewardCashWrapper}>
+              <div className={styles.rewardSummary}>
+                <Image src={listCashIcon} alt="cash-icon" width={15} />
+                <p>
+                  캐시 적립 혜택<span>|</span>최대{" "}
+                  <strong>{product.price / 10}원 적립</strong>
+                </p>
+              </div>
+
+              <div className={styles.rewardCashPromotion}>
+                <p>쿠페이 머니 결제 시 1% 적립</p>
+                <p>[로켓와우 + 쿠페이 계좌이체] 결제 시 2% 적립</p>
+                <p>[로켓와우 + 쿠페이 머니] 결제 시 4% 추가적립</p>
+                <button>로켓와우 무료체험 신청하기</button>
+              </div>
+            </div>
+
+            <Divider space={0} />
+
+            <div className={styles.bottom}>
+              <div className={styles.price}>{product.price * count}원</div>
+              <div className={styles.count}>
+                <Button
+                  onClick={() => setCount((prev) => prev - 1)}
+                  disabled={count > 1 ? false : true}
+                  secondary
+                >
+                  -
+                </Button>
+
+                <p>
+                  <b>{count}</b>
+                </p>
+
+                <Button onClick={() => setCount((prev) => prev + 1)} secondary>
+                  +
+                </Button>
+
+                <Button onClick={addToCart}>장바구니</Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
