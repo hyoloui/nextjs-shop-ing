348cb5abc93b3a214a4d0f4ebf4c78a48f5a0122
hyoloui <tmdgy9272@gmail.com>
2023-09-12 23:44:03 +0900

    fix: 결제 성공 요약 페이지 ( checkout-success page )
    - next fetch를 이용한 ssr 페이지 를 생성합니다
    - scss로 스타일링 합니다

--- src/app/(checkout)/checkout-success/page.jsx
+++ src/app/(checkout)/checkout-success/page.jsx
@@ -1,7 +1,56 @@
-import React from "react";
+import styles from "./CheckoutSuccess.module.scss";
 
-const CheckoutSuccess = () => {
-  return <div>CheckoutSuccess</div>;
+import Link from "next/link";
+
+import Heading from "@/components/heading/Heading";
+import Button from "@/components/button/Button";
+
+import { priceFormat } from "@/utils/priceFormat";
+import { formatTime } from "@/utils/day";
+
+const CheckoutSuccess = async ({ searchParams }) => {
+  const secretKey = process.env.NEXT_PUBLIC_TOSS_SECRET_KEY;
+
+  const url = `https://api.tosspayments.com/v1/payments/orders/${searchParams.orderId}`;
+  const basicToken = Buffer.from(`${secretKey}:`, "utf-8").toString("base64");
+
+  const payment = await fetch(url, {
+    headers: {
+      Authorization: `Basic ${basicToken}`,
+      ContentType: "application/json",
+    },
+  }).then((res) => res.json());
+
+  const { card } = payment;
+
+  return (
+    <section className={styles.success}>
+      <Heading title="결제 성공" />
+      <ul className={styles.list}>
+        <li>
+          <b>결제 상품: {payment.orderName}</b>
+        </li>
+        <li>
+          <b>주문 번호: {payment.orderId}</b>
+        </li>
+        <li>
+          <b>
+            카드 번호: {card ? card.number : "카드 번호가 확인되지 않습니다"}
+          </b>
+        </li>
+        <li>
+          <b>결제 금액: {priceFormat(payment.totalAmount)}원</b>
+        </li>
+        <li>
+          <b>결제 승인날짜: {formatTime(payment.approveAt)}</b>
+        </li>
+      </ul>
+
+      <Button>
+        <Link href="/order-history">주문 상태 보기</Link>
+      </Button>
+    </section>
+  );
 };
 
 export default CheckoutSuccess;
