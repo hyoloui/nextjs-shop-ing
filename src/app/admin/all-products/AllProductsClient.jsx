d24fbb9ca9d39933c47e413a04e073d0e5c2a281
hyoloui <tmdgy9272@gmail.com>
2023-09-25 01:24:57 +0900

    feat: 관리자 상품 삭제하기 ( AllProductsClient )
    - 관리자 페이지의 총상품 페이지에 표시된 상품을 삭제하는 로직을 작성합니다

--- src/app/admin/all-products/AllProductsClient.jsx
+++ src/app/admin/all-products/AllProductsClient.jsx
@@ -9,10 +9,16 @@ import {
   selectFilteredProducts,
 } from "@/redux/slice/filterSlice";
 
+import { deleteDoc, doc } from "firebase/firestore";
+import { db } from "@/firebase/firebase";
+import { storage } from "@/firebase/firebase";
+
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { FaEdit, FaTrashAlt } from "react-icons/fa";
+import Notiflix from "notiflix";
+import { toast } from "react-toastify";
 
 import Loader from "@/components/loader/Loader";
 import Heading from "@/components/heading/Heading";
@@ -21,6 +27,7 @@ import Pagenation from "@/components/pagination/Pagination";
 
 import useFetchCollection from "@/hooks/useFetchCollection";
 import { priceFormat } from "@/utils/priceFormat";
+import { deleteObject, ref } from "firebase/storage";
 
 const AllProductsClient = () => {
   const [search, setSearch] = useState("");
@@ -58,7 +65,40 @@ const AllProductsClient = () => {
     );
   }, [search, products, dispatch]);
 
-  const confirmDelete = () => {};
+  const confirmDelete = (id, imageURL) => {
+    Notiflix.Confirm.show(
+      "상품 삭제",
+      "이 상품을 삭제하시겠습니까?",
+      "삭제",
+      "취소",
+      function ok() {
+        deleteProduct(id, imageURL);
+      },
+      function cancel() {
+        return;
+      },
+      {
+        width: "320px",
+        borderRadius: "3px",
+        titleColor: "#4385F4",
+        okButtonBackground: "#4385F4",
+        cssAnimationStyle: "zoom",
+      }
+    );
+
+    const deleteProduct = async (id, imageURL) => {
+      try {
+        await deleteDoc(doc(db, "products", id));
+
+        const storageRef = ref(storage, imageURL);
+        await deleteObject(storageRef);
+
+        toast.success("상품이 삭제되었습니다.");
+      } catch (error) {
+        toast.error(error.message);
+      }
+    };
+  };
 
   return (
     <>
@@ -93,7 +133,10 @@ const AllProductsClient = () => {
                 const { id, name, category, price, imageURL } = product;
                 return (
                   <tr key={id}>
+                    {/* 순서 */}
                     <td>{index + 1}</td>
+
+                    {/* 이미지 */}
                     <td>
                       <Image
                         src={imageURL}
@@ -102,9 +145,17 @@ const AllProductsClient = () => {
                         height={100}
                       />
                     </td>
+
+                    {/* 이름 */}
                     <td>{name}</td>
+
+                    {/* 카테고리 */}
                     <td>{category}</td>
+
+                    {/* 가격 */}
                     <td>{priceFormat(price)}원</td>
+
+                    {/* 실행 */}
                     <td>
                       <Link href={`/admin/edit-product/${id}`}>
                         <FaEdit size={20} color="green" />
