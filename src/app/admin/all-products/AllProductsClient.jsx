ed8a26a6c8567dd716207ab19c35e10fa37f701d
hyoloui <tmdgy9272@gmail.com>
2023-09-24 19:33:09 +0900

    feat: 관리자 모든 상품 페이지 ( AllProductsClient )
    - 모든 상품 페이지 UI 생성합니다, 페이지 네이션 포함

--- src/app/admin/all-products/AllProductsClient.jsx
+++ src/app/admin/all-products/AllProductsClient.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-// import styles from "./AllProductsClient.module.scss";
+import styles from "./AllProducts.module.scss";
 
 import { useDispatch, useSelector } from "react-redux";
 import { STORE_PRODUCTS, selectProducts } from "@/redux/slice/productSlice";
@@ -9,8 +9,18 @@ import {
   selectFilteredProducts,
 } from "@/redux/slice/filterSlice";
 
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
+
+import Loader from "@/components/loader/Loader";
+import Heading from "@/components/heading/Heading";
+import Search from "@/components/search/Search";
+import Pagenation from "@/components/pagination/Pagination";
+
 import useFetchCollection from "@/hooks/useFetchCollection";
-import { useEffect } from "react";
+import { priceFormat } from "@/utils/priceFormat";
 
 const AllProductsClient = () => {
   const [search, setSearch] = useState("");
@@ -48,7 +58,79 @@ const AllProductsClient = () => {
     );
   }, [search, products, dispatch]);
 
-  return <div>AllProductsClient</div>;
+  const confirmDelete = () => {};
+
+  return (
+    <>
+      {isLoading && <Loader />}
+      <div className={styles.table}>
+        <Heading
+          title="모든 상품"
+          subtitle={`${filteredProducts.length} 개의 상품`}
+        />
+
+        <div className={styles.search}>
+          <Search value={search} onChange={(e) => setSearch(e.target.value)} />
+        </div>
+
+        {currentProducts.length === 0 ? (
+          <p>상품이 없습니다.</p>
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                <th>순서</th>
+                <th>이미지</th>
+                <th>이름</th>
+                <th>카테고리</th>
+                <th>가격</th>
+                <th>실행</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {currentProducts.map((product, index) => {
+                const { id, name, category, price, imageURL } = product;
+                return (
+                  <tr key={id}>
+                    <td>{index + 1}</td>
+                    <td>
+                      <Image
+                        src={imageURL}
+                        alt={name}
+                        width={100}
+                        height={100}
+                      />
+                    </td>
+                    <td>{name}</td>
+                    <td>{category}</td>
+                    <td>{priceFormat(price)}원</td>
+                    <td>
+                      <Link href={`/admin/edit-product/${id}`}>
+                        <FaEdit size={20} color="green" />
+                      </Link>{" "}
+                      <FaTrashAlt
+                        size={18}
+                        color="red"
+                        onClick={() => confirmDelete(id, imageURL)}
+                      />
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
+
+        <Pagenation
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          totalProducts={filteredProducts.length}
+          productsPerPage={productsPerPage}
+        />
+      </div>
+    </>
+  );
 };
 
 export default AllProductsClient;
