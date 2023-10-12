18d32b34746622fca96a1bfcbc25b0efc8b4ec72
hyoloui <tmdgy9272@gmail.com>
2023-10-13 00:51:59 +0900

    fix: debug Header
    - 로그인 상태를 확인하여 조건부 렌더링 하도록 합니다

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -2,6 +2,13 @@
 
 import styles from "./Header.module.scss";
 
+import { useDispatch, useSelector } from "react-redux";
+import {
+  REMOVE_ACTIVE_USER,
+  SET_ACTIVE_USER,
+  selectIsLoggedIn,
+} from "@/redux/slice/authSlice";
+
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
@@ -11,20 +18,18 @@ import { auth } from "@/firebase/firebase";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 
 import InnerHeader from "../innerHeader/InnerHeader";
-import { useDispatch } from "react-redux";
-import { REMOVE_ACTIVE_USER, SET_ACTIVE_USER } from "@/redux/slice/authSlice";
 
 const Header = () => {
   const router = useRouter();
   const pathname = usePathname();
   const dispatch = useDispatch();
-
   const [displayName, setDisplayName] = useState("");
+  const isLoggedIn = useSelector(selectIsLoggedIn);
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
-        if (user.displayName == null) {
+        if (user.displayName === null) {
           const u1 = user.email!.substring(0, user.email!.indexOf("@"));
           const uName = u1.charAt(0).toUpperCase() + u1.slice(1);
           setDisplayName(uName);
@@ -69,36 +74,38 @@ const Header = () => {
     <header>
       <div className={styles.loginBar}>
         <ul className={styles.list}>
-          <>
+          {!isLoggedIn ? (
             <li className={styles.item}>
               <Link href={"/login"}>로그인</Link>
             </li>
+          ) : (
+            <>
+              <li className={styles.item}>
+                <Link href={"/admin/dashboard"}>관리자</Link>
+              </li>
 
-            <li className={styles.item}>
-              <Link href={"/admin/dashboard"}>관리자</Link>
-            </li>
+              <li className={styles.item}>
+                <Link href={"/order-history"}>주문 목록</Link>
+              </li>
+              <li className={styles.item}>
+                <Link href={"/"} onClick={logoutUser}>
+                  로그아웃
+                </Link>
+              </li>
 
-            <li className={styles.item}>
-              <Link href={"/order-history"}>주문 목록</Link>
-            </li>
-            <li className={styles.item}>
-              <Link href={"/"} onClick={logoutUser}>
-                로그아웃
-              </Link>
-            </li>
+              <li className={styles.item}>
+                <Link href={"/"}>제휴 마케팅</Link>
+              </li>
 
-            <li className={styles.item}>
-              <Link href={"/"}>제휴 마케팅</Link>
-            </li>
+              <li className={styles.item}>
+                <Link href={"/"}>쿠팡 플레이</Link>
+              </li>
 
-            <li className={styles.item}>
-              <Link href={"/"}>쿠팡 플레이</Link>
-            </li>
-
-            <li className={styles.item}>
-              <Link href={"/"}>고객센터</Link>
-            </li>
-          </>
+              <li className={styles.item}>
+                <Link href={"/"}>고객센터</Link>
+              </li>
+            </>
+          )}
         </ul>
       </div>
       {pathname.startsWith("/admin") ? null : <InnerHeader />}
